0431d522e2b462e2211d94455d220b9324e9d5a2
Sahil Rajput <sahilrajput03@gmail.com>
2021-04-25 13:04:32 +0530

    ~ sahil

---
+++ js.js
@@ -0,0 +1,6 @@
+var fs = require("fs");
+fs.writeFile("/test.txt", "Cool, I can do this in the browser!", function (err) {
+  fs.readFile("/test.txt", function (err, contents) {
+    console.log(contents.toString());
+  });
+});
